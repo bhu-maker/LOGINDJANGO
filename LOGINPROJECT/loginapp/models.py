82121296edb869735c0897baeeb7763712fd7a9a
bhu-maker <bhu_ess@yahoo.co.in>
2022-04-09 17:15:31 +0530

    otp

--- LOGINPROJECT/loginapp/models.py
+++ LOGINPROJECT/loginapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class logintable(models.Model):
@@ -6,5 +7,5 @@ class logintable(models.Model):
     password1=models.CharField(max_length=100)
     password2=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
-    phoneno=models.BigIntegerField()
+    phoneno=models.BigIntegerField(validators=[RegexValidator(r'^\d\d\d\d\d\d\d\d\d\d$')],help_text=("format:xxxxxxxxxx,required,unique"),verbose_name=("Contact No"))
 
